034a963b3ded6c34ee91106ad9035ceb6a84bd20
martinza99 <mrt.zacharias@gmail.com>
2019-02-11 18:17:05 +0100

    Update autoLoop.user.js

--- autoLoop.user.js
+++ autoLoop.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto loop video
 // @namespace    https://github.com/martinza99/browserEnhancements/
-// @version      0.1.1
+// @version      0.1
 // @description  enables loop on videos
 // @author       martin
 // @match        file:///*
@@ -13,7 +13,7 @@
     var allowedTypes = ['mp4','webm'];
     var filetype = document.location.href.substring(document.location.href.lastIndexOf('.')+1); // gets everything in the URL after the last dot '.'
     if(allowedTypes.includes(filetype)){
-      var video = document.getElementsByTagName('video')[0];//test
+      var video = document.getElementsByTagName('video')[0];
       video.loop = true;
     }
 
