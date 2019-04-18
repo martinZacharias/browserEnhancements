79125cdf9ebd2cafeff3fe3ecf06717a9577e5bb
martinza99 <mrt.zacharias@gmail.com>
2019-04-18 08:51:21 +0200

    added interval check
    added interval check in case of delayed auto fill / script loading

--- iliasAutoLogin.user.js
+++ iliasAutoLogin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ILIAS auto login
 // @namespace    https://github.com/martinza99/browserEnhancements/
-// @version      0.1
+// @version      0.2
 // @description  saves you the login buton click
 // @author       martin
 // @match        https://www.ilias.fh-dortmund.de/ilias/login.php*
@@ -13,4 +13,10 @@
     $("#password").change(function(){
         document.getElementsByName("cmd[doStandardAuthentication]")[0].click();
 });
+    setInterval(formSub, 3000);
 })();
+
+function formSub(){
+    if($("#password").val()!="")
+        document.getElementsByName("cmd[doStandardAuthentication]")[0].click();
+}
