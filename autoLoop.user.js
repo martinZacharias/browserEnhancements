394c87c3841bbd19b39ad4cb6c19bc8814568f24
martinza99 <mrt.zacharias@gmail.com>
2019-10-15 13:42:18 +0200

    added regex pattern to match parameter

--- autoLoop.user.js
+++ autoLoop.user.js
@@ -1,21 +1,17 @@
 // ==UserScript==
 // @name         Auto loop video
 // @namespace    https://github.com/martinza99/browserEnhancements/
-// @version      0.3
+// @updateURL    https://github.com/martinza99/browserEnhancements/raw/master/autoLoop.user.js
+// @version      0.4
 // @description  enables loop on videos
 // @author       martin
-// @match        file:///*
+// @include      /^file:///.*(mp4|webm)$/
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    var allowedTypes = ['mp4','webm'];
-    var filetype = document.location.href.substring(document.location.href.lastIndexOf('.')+1); // gets everything in the URL after the last dot '.'
-    if(allowedTypes.includes(filetype)){
-      var video = document.getElementsByTagName('video')[0];
-      video.loop = true;
-      video.volume=0.15;
-    }
-
+    var video = document.getElementsByTagName('video')[0];
+    video.loop = true;
+    video.volume=0.15;
 })();
