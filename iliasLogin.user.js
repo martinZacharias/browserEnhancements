a62047cc80fcc16effa971b6f0975b3bb0fb0f05
martinza99 <mrt.zacharias@gmail.com>
2019-11-11 15:47:53 +0100

    uploaded auto login userscript
    saves you a few clicks on the login buton click

---
+++ iliasLogin.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         ILIAS auto login
+// @namespace    https://github.com/martinza99/browserEnhancements/
+// @updateURL    https://github.com/martinza99/browserEnhancements/raw/master/iliasLogin.user.js
+// @version      0.2.1
+// @description  saves you the login buton click
+// @author       martinZa99
+// @match        https://www.ilias.fh-dortmund.de/ilias/login.php*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+	'use strict';
+	$("#password").change(function(){
+		document.getElementsByName("cmd[doStandardAuthentication]")[0].click();
+});
+	setInterval(formSub, 200); //waiting for password autofill
+})();
+
+function formSub(){
+	if($("#password").val()!="")
+		document.getElementsByName("cmd[doStandardAuthentication]")[0].click();
+}
