a266a617bd7dfa1a31acc497fbd310cfa4a29941
martinza99 <mrt.zacharias@gmail.com>
2019-11-26 09:09:13 +0100

    replaced hardcoded substring with regex match

--- iliasFixTitle.user.js
+++ iliasFixTitle.user.js
@@ -2,7 +2,7 @@
 // @name         ILIAS title fix
 // @namespace    https://github.com/martinza99/browserEnhancements/
 // @updateURL    https://github.com/martinza99/browserEnhancements/raw/master/iliasFixTitle.user.js
-// @version      0.2
+// @version      0.3
 // @description  flips and shortens the title on most ILIAS pages from "ILIAS - E-Learning an der FH Dortmund - PAGENAME" to "PAGENAME - ILIAS"
 // @author       martinZa99
 // @match        https://www.ilias.fh-dortmund.de/*
@@ -11,6 +11,7 @@
 
 (function() {
     'use strict';
-    if(document.title.substr(0,40) == "ILIAS - E-Learning an der FH Dortmund - ")
-        document.title = document.title.substr(40) + " - ILIAS"
+    let match = document.title.match(".*ILIAS - E-Learning an der FH Dortmund - (.*)");
+    if(match != undefined) // in case some pages don't match the pattern
+        document.title = match[1] + " - ILIAS"
 })();
