ea8068ddc2a775ac191fbb6ea9230744eda67de4
martinza99 <mrt.zacharias@gmail.com>
2020-07-03 03:05:41 +0200

    added offset for PDF navbar

--- pdfBoxes.user.js
+++ pdfBoxes.user.js
@@ -2,7 +2,7 @@
 // @name         Draw boxes on PDFs
 // @namespace    https://github.com/martinza99/browserEnhancements/
 // @updateURL    https://github.com/martinza99/browserEnhancements/raw/master/pdfBoxes.user.js
-// @version      0.1
+// @version      0.2
 // @description  adds drawable boxes to PDFs
 // @author       martinZa99
 // @include      /\.pdf$/
@@ -12,9 +12,11 @@
 (function () {
 	const canvas = document.createElement("canvas");
 	canvas.width = innerWidth - 100;
-	canvas.height = innerHeight;
+	canvas.height = innerHeight - 50;
 	canvas.style.opacity = 0.8;
 	canvas.style.borderRight = "2px solid darkgoldenrod";
+	canvas.style.position = "absolute";
+	canvas.style.top = "50px";
 
 	const ctx = canvas.getContext("2d");
 	document.body.append(canvas);
@@ -27,15 +29,17 @@
 	});
 
 	canvas.addEventListener("click", (event) => {
+		const x = event.x;
+		const y = event.y - 50;
 		if (start == null) {
-			start = { x: event.x, y: event.y };
+			start = { x, y };
 			ctx.fillStyle = "red";
-			ctx.fillRect(start.x, start.y, 5, 5);
+			ctx.fillRect(x, y, 5, 5);
 			ctx.fillStyle = "black";
 		} else {
 			ctx.fillRect(0, 0, canvas.width, canvas.height);
-			const w = event.x - start.x;
-			const h = event.y - start.y;
+			const w = x - start.x;
+			const h = y - start.y;
 			ctx.clearRect(start.x, start.y, w, h);
 			start = null;
 		}
