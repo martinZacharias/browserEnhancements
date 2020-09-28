40e1febb6085112fb458a374f2518efbf85d3fd5
martinza99 <mrt.zacharias@gmail.com>
2020-09-28 12:25:44 +0200

    added highlighting to Notenspiegel

--- odsHighlights.user.js
+++ odsHighlights.user.js
@@ -2,18 +2,42 @@
 // @name         ODS highlight passed
 // @namespace    https://github.com/martinza99/browserEnhancements/
 // @updateURL    https://github.com/martinza99/browserEnhancements/raw/master/odsHighlights.user.js
-// @version      0.1
+// @version      0.2
 // @description  highlights passed exams in ODS
 // @author       martinza99
-// @match        https://ods.fh-dortmund.de/ods?Sicht=ExcS&ExcSicht=PAnmeldung*
+// @match        https://ods.fh-dortmund.de/ods?Sicht=ExcS*
 // @grant        none
 // ==/UserScript==
 
 (function () {
 	"use strict";
-	const rows = document.querySelectorAll("table.standard tr");
-	for (const row of rows) {
-		if (row.lastElementChild.textContent == "BE")
-			row.style.backgroundColor = "lime";
+	const ExcSicht = new URLSearchParams(location.search).get("ExcSicht");
+	switch (ExcSicht) {
+		case "PAnmeldung": {
+			const rows = document.querySelectorAll("table.standard tr");
+			for (const row of rows) {
+				if (row.cells[8].textContent == "BE")
+					row.style.backgroundColor = "lime";
+			}
+			break;
+		}
+		case "Notenspiegel": {
+			const rowsMain = document.querySelectorAll(".small tr");
+			for (const row of rowsMain) {
+				switch (row.cells[7].textContent.substr(0, 2)) {
+					case "BE":
+						row.style.backgroundColor = "lime";
+						break;
+					case "NB":
+						row.style.backgroundColor = "salmon";
+						break;
+					default:
+						break;
+				}
+			}
+			break;
+		}
+		default:
+			break;
 	}
 })();
